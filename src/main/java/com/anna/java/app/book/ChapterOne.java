a8b4543eef7e2be1dc29a937ac06c55b58180c4f
Anna Wieclaw <awieclaw@ford.com>
2021-03-29 01:34:04 +0100

    Learn java Chapter One

---
+++ src/main/java/com/anna/java/app/book/ChapterOne.java
@@ -0,0 +1,14 @@
+package com.anna.java.app.book;
+
+public class ChapterOne {
+    /*coommet
+    * javac ChapterOne.java -> will complie this ffile to bytecode
+    * java ChapterOne - runs it
+    *  */
+
+    //LAunching single file source code programs: java ChapterOne.java Hello - just in memory, doesnt create .class file
+
+    public static void main(String[] args) {
+        System.out.println("Single file: " + args[0]);
+    }
+}
